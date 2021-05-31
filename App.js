83de8479d9906a5ed8302e9324cf476f18782c07
ahmed <faozy1234@gmail.com>
2021-05-31 14:07:16 +0200

    added overview, rrating and title

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import React, { useState, useEffect, createRef} from 'react'
+import { StyleSheet, Text, View, Image, ScrollView, TouchableHighlight} from 'react-native';
 import Header from './components/Header'
 export default function App() {
   const API_URL = "http://api.themoviedb.org/3/discover/movie?api_key=59f0bcf1898b36006f068c2e060b0064"
   const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
   const [movies, setMovies] = useState([])
- 
+  const container = createRef(null);
+
   useEffect(() =>{
     getMovies(API_URL)
 
@@ -23,7 +24,10 @@ export default function App() {
         let obj = {
           id: e.id,
           title: e.original_title,
-          poster_path :e.poster_path
+          poster_path :e.poster_path,
+          overview: e.overview,
+          rating: e.vote_average
+
         }
         list.push(obj)
 
@@ -36,14 +40,25 @@ export default function App() {
     <View style={styles.container}>
       <View >
         <Text style={styles.title}> Movie Database !</Text>
-       <ScrollView style={styles.results}> 
+       <ScrollView 
+        style={styles.results}
+        bounces={false}
+        scrollEnabled={true}> 
          {movies.map(movie => (
+           <TouchableHighlight 
+           >
             <View  key={movie.id} style={styles.result}>
-                <Text style={styles.heading}>{movie.title}</Text>
                 <Image style={styles.poster}
                 source={{uri: IMG_URL+movie.poster_path}}
-                />      
+                />
+                <View style={styles.movieInfo}>
+                    <Text style={styles.heading}>{movie.title}</Text>
+                    <Text style={styles.rating} >{movie.rating}</Text>
+                    <View style={styles.overview}> {movie.overview} </View>      
+
+                </View>
             </View>
+            </TouchableHighlight>
          ))}
        </ScrollView>
 
@@ -73,7 +88,7 @@ const styles = StyleSheet.create({
   },
   result:{
     flex:1,
-    width:'100',
+    width:'100%',
     marginBottom:20
   },
   heading:{
@@ -86,6 +101,36 @@ const styles = StyleSheet.create({
   poster:{
     width:'100%',
     height: 300
-  }
+  },
+  overview:{
+    backgroundColor:'#fff',
+    fontWeight:'200',
+    fontSize:20,
+    color: '#000',
+    position:'absolute',
+    padding:25,
+    
+  },
+  movieInfo:{
+    color:'#eee',
+    backgroundColor:'#445565',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    letterSpacing: '0.5px',
+    paddingRight:80
+},
+  rating:{
+  color:'#fff',
+  width:30,
+  height:30,
+  backgroundColor: '#223343',
+  borderRadius: '5px',
+  fontWeight: "700",
+  textAlign:'center',
+  fontSize:15,
+  paddingTop:5
+}
 
 });
